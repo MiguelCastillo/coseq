cef7940508980932c8fb2941c3aba3b6c493be21
Miguel Castillo <mcastillo@symphono.com>
2016-12-30 00:24:52 -0500

    adding initial async sequence unit tests

---
+++ test/specs/asyncSpec.js
@@ -0,0 +1,133 @@
+import coseq from '../../index';
+import { expect } from 'chai';
+
+var asyncIterator;
+
+describe('async sequence suite', function() {
+  describe('Given an async iterator', () => {
+    var configureGenerator = () => {
+      async function* getItemsAsync() {
+        for (var i = 1; i <= 5; i++) {
+          yield await Promise.resolve(i);
+        }
+
+        return "YES!!";
+      }
+
+      asyncIterator = getItemsAsync();
+    };
+
+    describe('when the iterator generates 5 items', () => {
+      before(() => {
+        configureGenerator();
+      });
+
+      assertNextValue(1, false);
+      assertNextValue(2, false);
+      assertNextValue(3, false);
+      assertNextValue(4, false);
+      assertNextValue(5, false);
+      assertNextValue('YES!!', true);
+      assertNextValue(undefined, true);
+    });
+
+    describe('and calling iterator.next', () => {
+      var iter;
+      before(() => {
+        configureGenerator();
+        iter = asyncIterator.next();
+      });
+
+      it('then iter.next returns a promise', () => {
+        expect(iter).to.be.a('promise');
+      });
+    });
+
+    describe('And filtering even numbers', () => {
+      before(() => {
+        configureGenerator();
+
+        asyncIterator = coseq
+          .asyncSequence(asyncIterator)
+          .filter(value => value % 2 === 0)
+          .iterator();
+      });
+
+      assertNextValue(2, false);
+      assertNextValue(4, false);
+      assertNextValue('YES!!', true);
+      assertNextValue(undefined, true);
+    });
+
+    describe('And multiplying even numbers by 4', () => {
+      before(() => {
+        configureGenerator();
+
+        asyncIterator = coseq
+          .asyncSequence(asyncIterator)
+          .filter(value => value % 2 === 0)
+          .map(value => value * 4)
+          .iterator();
+      });
+
+      assertNextValue(8, false);
+      assertNextValue(16, false);
+      assertNextValue('YES!!', true);
+      assertNextValue(undefined, true);
+    });
+
+    describe('And skipping the first 2 items and multiplying even numbers by 3', () => {
+      before(() => {
+        configureGenerator();
+
+        asyncIterator = coseq
+          .asyncSequence(asyncIterator)
+          .skip(2)
+          .filter(value => value % 2 === 0)
+          .map(value => value * 3)
+          .iterator();
+      });
+
+      assertNextValue(12, false);
+      assertNextValue('YES!!', true);
+      assertNextValue(undefined, true);
+    });
+
+    describe('And skipping items until the value is 2', () => {
+      before(() => {
+        configureGenerator();
+
+        asyncIterator = coseq
+          .asyncSequence(asyncIterator)
+          .skipUntil(value => value === 2)
+          .iterator();
+      });
+
+      assertNextValue(2, false);
+      assertNextValue(3, false);
+      assertNextValue(4, false);
+      assertNextValue(5, false);
+      assertNextValue('YES!!', true);
+      assertNextValue(undefined, true);
+    });
+
+  });
+});
+
+
+function assertNextValue(value, done) {
+  var result;
+  describe('and calling next', () => {
+    before(async () => {
+      result = await asyncIterator.next();
+    });
+
+    it(`then iterator returns ${value}`, () => {
+      expect(result.value).to.equal(value);
+    });
+
+    it(`then iterator is ${done ? "" : "not"} done`, () => {
+      expect(result.done).to.equal(done);
+    });
+  });
+}
