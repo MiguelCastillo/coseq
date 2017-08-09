472785e0f48eb69f97d493d34576677e87bb06f8
Miguel Castillo <mcastillo@symphono.com>
2017-08-08 21:22:42 -0400

    updated how action sequences are built to make the logic much much simpler to work with.

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ class Action {
   }
 
   forEach(cb) {
-    var sequence = buildActionSequence(new ForEachAction(this, cb)).withStrategy(asyncSequence);
+    var sequence = buildActionSequence(new ForEachAction(this, cb), asyncSequence);
 
     return new Promise(resolve => {
       (function iterateSequence(root) {
@@ -71,11 +71,11 @@ class Action {
    * Iterator API
    */
   iterator() {
-    return buildActionSequence(this).withStrategy(syncSequence);
+    return buildActionSequence(this, syncSequence);
   }
 
   asyncIterator() {
-    return buildActionSequence(this).withStrategy(asyncSequence);
+    return buildActionSequence(this, asyncSequence);
   }
 
   [Symbol.iterator]() {
@@ -84,19 +84,6 @@ class Action {
 
   [Symbol.asyncIterator]() {
     return this.asyncIterator();
-  }
-
-  /**
-   * experimental pull sequence where each action in the chain will
-   * ask its parent (preceeding) action for data. This is a recursive
-   * process in which the end result does not build a sequence chain
-   * and then execute. Instead the sequence is executed as the sequence
-   * is being built.
-   */
-  pull(nextIter) {
-    return this.prev ?
-      this.prev.pull(new ActionIterator((value) => this.exec(value, nextIter))) :
-      this.exec(undefined, nextIter);
   }
 }
 
@@ -197,99 +184,55 @@ class RootAction extends Action {
     this.dataIter = dataIter;
   }
 
-  withStrategy(strategy) {
-    this.strategy = strategy;
-    return this;
-  }
-
   exec(value, done) {
-    return this.strategy(this.dataIter, value, done);
+    done(this.dataIter.next(value));
   }
 }
 
-class ActionIterator {
-  constructor(action, next) {
-    this.action = action;
-    this.next = next;
-  }
+function buildActionSequence(action, strategy) {
+  var next = null;
 
-  withStrategy(strategy) {
-    if (!(this.action instanceof RootAction)) {
-      throw new TypeError("Only root actions can have a strategy");
-    }
-
-    this.action.withStrategy(strategy);
-    return this;
-  }
-}
-
-function buildActionSequence(action) {
-  var root;
-
-  /**
-   * This is walking the list of method that process data backwards
-   * to create a sequence that can be executed by pushing data from
-   * one action to the next.
-   */
   while (action) {
-    root = new ActionIterator(action, createNext(action, root));
+    next = { next, action };
     action = action.prev;
   }
 
-  return root;
-
-  /**
-   * The flow is relatively tanggled up. But the idea is that every
-   * action in a chain, which together form a sequence of actions
-   * can push values on from one action to the next by queueing up
-   * their results. The result is queued in a RootAction, which knows
-   * how to handle those new results. The reason for that is to ensure
-   * we keep the stack as shallow as possible when executing the
-   * sequence of actions.
-   *
-   * Building the sequence of actions also relies on the closure object
-   * to keep around easy access to the RootAction so that we can do
-   * fancy things like skip or filter items.
-   */
-  function createNext(action, iter) {
-    return function next(value, queueResult = (v) => v) {
-      return action.exec(value, function doneCallback(result) {
-        return queueResult({
-          done: !!result.done,
-          iter: result.skip ? root : iter,
-          value: result.value
-        });
-      });
-    };
-  }
+  return {
+    next: (value) => {
+      return strategy(next, value);
+    }
+  };
 }
 
-function syncSequence(dataIter, value, init) {
-  var result = dataIter.next(value);
-  return result.done ? init(result) : pushValue(init(result));
+function syncSequence(root, value) {
+  var next = root;
+  var result = { value };
 
-  function pushValue(result) {
-    while (result && result.iter && !result.done) {
-      result.iter.next(result.value, (nextResult) => { result = nextResult; });
-    }
-
-    return result;
-  }
-}
-
-function asyncSequence(dataIter, value, init) {
-  return Promise
-    .resolve(dataIter.next(value))
-    .then((result) => result.done ? init(result) : pushValue(init(result)));
-
-  function pushValue(result) {
-    if (!result || !result.iter) {
-      return result;
-    }
-
-    return new Promise(resolve => result.iter.next(result.value, resolve)).then(result => {
-      return result.done ? result : pushValue(result);
+  while(!result.done && next) {
+    next.action.exec(result.value, (r) => {
+      result = r.skip ? { value } : r;
+      next = r.skip ? root : next.next;
     });
+  }
+
+  result.done = !!result.done;
+  return result;
+}
+
+function asyncSequence(root, value) {
+  return nextValue(root, { value });
+
+  function nextValue(next, result) {
+    if (!next || result.done) {
+      result.done = !!result.done;
+      return Promise.resolve(result);
+    }
+
+    return new Promise(resolve => next.action.exec(result.value, resolve))
+      .then(result => (
+        result.done ? result :
+        result.skip ? nextValue(root, { value }) : nextValue(next.next, result)
+      ));
   }
 }
 
